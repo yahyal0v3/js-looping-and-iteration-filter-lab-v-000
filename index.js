ad49a273c9fa7f4d320923bddfc87088e9fcff25
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-01 01:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMatching(collection, match) {
-  return collection.fitler(function (element) { return element === match })
+function findMatching(drivers, match) {
+  return drivers.fitler(function (element) { return element === match })
 }
