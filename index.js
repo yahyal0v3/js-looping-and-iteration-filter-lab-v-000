c6d9f8c6c6a818b5025a37e29b91b87135d96e12
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-01 01:42:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMatching(array, match) {
-  return array.fitler(function (element) { return element === match })
+function findMatching(collection, match) {
+  return collection.fitler(function (element) { return element === match })
 }
