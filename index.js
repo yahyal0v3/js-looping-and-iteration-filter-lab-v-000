cbfc376961bfd4446e17e8aeb095cab5550dafb9
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-01 01:54:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file
-
-
 function findMatching(collection, match) {
-  return collection.filter(element => element.toLowerCase() === match.toLowerCase())
+  return collection.fitler(element => element.toLowerCase() === match.toLowerCase())
 }
+
