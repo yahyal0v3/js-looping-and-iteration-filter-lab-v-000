edb7ef1a33f86cfee8334c76489815de83d4620c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-01 01:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ function findMatching(collection, match) {
   return collection.fitler(element => element.toLowerCase() === match.toLowerCase() )
 }
 
-function findMatching(collection, match) {
-  return collection.filter(element => element.toLowerCase() === match.toLowerCase())
-}
+
